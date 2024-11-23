b170ba8335cbebe560c548612372940797d88d3d
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-11-23 11:53:18 +0800

    chore: todo admin.

--- quickstart/todo/admin/todo.py
+++ quickstart/todo/admin/todo.py
@@ -15,11 +15,28 @@ class TodoAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
         "is_in_progress",
+        "is_completed",
+        "is_cancelled",
     ]
 
     list_filter = [
         "user",
     ]
+
+    @admin.display(boolean=True)
+    def is_in_progress(self, obj):
+        """Check if in progress"""
+        return obj.is_in_progress
+
+    @admin.display(boolean=True)
+    def is_completed(self, obj):
+        """Check if completed"""
+        return obj.is_completed
+
+    @admin.display(boolean=True)
+    def is_cancelled(self, obj):
+        """Check if cancelled"""
+        return obj.is_cancelled
 
 
 admin.site.register(Todo, TodoAdmin)
