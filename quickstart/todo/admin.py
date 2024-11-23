9c138fc6221ad6dc403904fd3f2ca869ed97abf8
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-11-23 10:55:55 +0800

    chore: todo admin.

--- quickstart/todo/admin.py
+++ quickstart/todo/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 from todo.models import Todo
 
-admin.site.register(Todo)
+
+class TodoAdmin(admin.ModelAdmin):
+    """Todo model admin."""
+
+    list_display = [
+        "id",
+        "title",
+        "description",
+        "user",
+        "deadline",
+        "created_at",
+        "updated_at",
+        "is_completed",
+        "is_abandoned",
+    ]
+
+    list_filter = ["user", "is_completed", "is_abandoned"]
+
+
+admin.site.register(Todo, TodoAdmin)
