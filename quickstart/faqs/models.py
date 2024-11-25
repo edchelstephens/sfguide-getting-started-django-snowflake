f5752adc2d7dbfe94b7f21636f68471df8459fa9
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-11-25 11:06:11 +0800

    chore: migration - new model fields.

--- quickstart/faqs/models.py
+++ quickstart/faqs/models.py
@@ -8,6 +8,8 @@ class Question(models.Model):
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     question = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
 
 class Answer(models.Model):
@@ -16,3 +18,5 @@ class Answer(models.Model):
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     question = models.ForeignKey("faqs.Question", on_delete=models.CASCADE)
     answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
