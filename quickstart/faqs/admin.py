842c74cbdd10a673babedf59e6432ed1c0233ade
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-11-25 11:09:12 +0800

    chore: admin classes.

--- quickstart/faqs/admin.py
+++ quickstart/faqs/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from faqs.models import Question, Answer
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    """Question model admin."""
+
+    list_display = [
+        "id",
+        "user",
+        "question",
+        "deadline",
+        "status",
+        "created_at",
+        "updated_at",
+    ]
+
+    list_filter = ["user"]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    """Answer model admin."""
+
+    list_display = [
+        "id",
+        "question",
+        "answer",
+        "created_at",
+        "updated_at",
+    ]
+
+    list_filter = ["question"]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
