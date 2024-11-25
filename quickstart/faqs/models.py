2c9f72108491a4d292fd48a8dc7c7ae89552e21b
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-11-25 11:04:27 +0800

    chore: faqs answer.

--- quickstart/faqs/models.py
+++ quickstart/faqs/models.py
@@ -8,3 +8,11 @@ class Question(models.Model):
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     question = models.TextField()
+
+
+class Answer(models.Model):
+    """Answer model."""
+
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
+    question = models.ForeignKey("faqs.Question", on_delete=models.CASCADE)
+    answer = models.TextField()
