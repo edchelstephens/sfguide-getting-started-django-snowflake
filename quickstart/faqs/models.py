890ac4e5c484d16a7aa86317224d42b888ba7828
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-11-25 11:27:36 +0800

    chore: timestamped abstract models.

--- quickstart/faqs/models.py
+++ quickstart/faqs/models.py
@@ -2,28 +2,34 @@ from django.db import models
 from uuid import uuid4
 
 
-class Question(models.Model):
+class TimeStampedModel(models.Model):
+    """Timestamped model."""
+
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+
+class Question(TimeStampedModel):
     """Question model."""
 
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     question = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         """Human readable object string representation."""
         return self.question
 
 
-class Answer(models.Model):
+class Answer(TimeStampedModel):
     """Answer model."""
 
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
     question = models.ForeignKey("faqs.Question", on_delete=models.CASCADE)
     answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         """Human readable object string representation."""
