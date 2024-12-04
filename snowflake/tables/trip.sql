7e60ce12e89c45785682ea57fc9fe705e865a79b
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-12-04 08:37:01 +0800

    chore: trip sql table definition.

---
+++ snowflake/tables/trip.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE TABLE SNOWFLAKE_DJANGO.BACKEND.TRIP(
+    trip_id integer autoincrement start 1 increment 1,
+    tripduration timestamp,
+    starttime timestamp,
+    stoptime timestamp,
+    start_station_id integer,
+    start_station_name string,
+    start_station_latitude float,
+    start_station_longitude float,
+    end_station_id integer,
+    end_station_name string,
+    end_station_latitude float,
+    end_station_longitude float,
+    bikeid integer,
+    membership_type string,
+    usertype string,
+    birth_year integer,
+    gender integer
+);
