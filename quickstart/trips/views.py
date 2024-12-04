78165ec483b7da90d2c761aa3ee91779dafd6caa
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-12-04 09:14:07 +0800

    refactor: clean module

--- quickstart/trips/views.py
+++ quickstart/trips/views.py
@@ -4,15 +4,15 @@ from django.views import generic
 
 from .models import Trip
 
-# Create your views here.
 
 def index(request):
     return HttpResponse("Hello there! Welcome to the django-snowflake Quickstart!")
 
+
 class GrahamConselyeaView(generic.ListView):
-    template_name = 'trips/trips-from-graham-conselyea.html'
-    context_object_name = 'trip_list'
+    template_name = "trips/trips-from-graham-conselyea.html"
+    context_object_name = "trip_list"
 
     def get_queryset(self):
         """Return the last 10 trips from the 'Graham Ave & Conselyea St' Citibike station."""
-        return Trip.objects.filter(start_station_name='Graham Ave & Conselyea St')[:10]
+        return Trip.objects.filter(start_station_name="Graham Ave & Conselyea St")[:10]
