cd42bd89db0264bc5ac86521dd6767858d68b6b2
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-12-04 09:10:28 +0800

    chore: restore old url patterns.

--- quickstart/quickstart/urls.py
+++ quickstart/quickstart/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", admin.site.urls),
     path("trips/", include("trips.urls")),
+    path("admin/", admin.site.urls),
 ]
