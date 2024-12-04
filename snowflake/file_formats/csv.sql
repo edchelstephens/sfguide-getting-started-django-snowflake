bb4c45236fa4fa45ff5ad73e608b0f62281a2196
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-12-04 08:42:54 +0800

    chore: cvs file format.

--- snowflake/file_formats/csv.sql
+++ snowflake/file_formats/csv.sql
@@ -1,6 +1,6 @@
 create or replace file format csv type='csv'
-    compression = 'auto' field_delimiter = ',' record_delimiter = '\n'
-    skip_header = 0 field_optionally_enclosed_by = '\042' trim_space = false
-    error_on_column_count_mismatch = false escape = 'none' escape_unenclosed_field = '\134'
-    date_format = 'auto' timestamp_format = 'auto' null_if = ('')
-    comment = 'file format named csv for ingesting data into snowflake';
+  compression = 'auto' field_delimiter = ',' record_delimiter = '\n'
+  skip_header = 0 field_optionally_enclosed_by = '\042' trim_space = false
+  error_on_column_count_mismatch = false escape = 'none' escape_unenclosed_field = '\134'
+  date_format = 'auto' timestamp_format = 'auto' null_if = ('')
+  comment = 'defined cvs file format nfor ingesting data into snowflake';
