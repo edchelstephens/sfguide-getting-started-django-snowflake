62d5c6f342c93c8dccc89c7935d1c1df6568360b
Edchel Stephen Nini <edchelstephens@gmail.com>
2024-12-04 08:43:35 +0800

    chore: csv format.

--- snowflake/file_formats/csv.sql
+++ snowflake/file_formats/csv.sql
@@ -1,4 +1,4 @@
-create or replace file format csv type='csv'
+create or replace file format snowflake_django.backend.csv type='csv'
   compression = 'auto' field_delimiter = ',' record_delimiter = '\n'
   skip_header = 0 field_optionally_enclosed_by = '\042' trim_space = false
   error_on_column_count_mismatch = false escape = 'none' escape_unenclosed_field = '\134'
